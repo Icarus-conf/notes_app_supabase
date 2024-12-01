e505079a5affdeab4c595c43e60470ae80353192
Icarus-conf <brain.m.config@gmail.com>
2024-12-01 16:24:35 +0200

    fixing minor issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app_supabase/data/api_cred.dart';
 import 'package:notes_app_supabase/data/auth_gate.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'https://kxzdrnjbjtmkbixpozsl.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4emRybmpianRta2JpeHBvenNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIzOTEyNDcsImV4cCI6MjA0Nzk2NzI0N30.ERVyFmPCZ5S-Det4UAt7BedekZo4XubJ8jpLUs2ZByc',
+    url: ApiCred.urlKey,
+    anonKey: ApiCred.apiKey,
   );
   runApp(const MyApp());
 }
